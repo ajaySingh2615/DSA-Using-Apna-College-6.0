2df707b846d1a7d11cba82651325cb504517d563
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-29 12:13:39 +0530

    Add LargestNumber, LeapYear, and OddEvenNumbers classes

---
+++ src/com/conditional/LargestNumber.java
@@ -0,0 +1,20 @@
+package com.conditional;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1 + " is greater than " + num2);
+        }else{
+            System.out.println(num2 + " is greater than " + num1);
+        }
+
+        scanner.close();
+    }
+}
---
+++ src/com/conditional/OddEvenNumbers.java
@@ -0,0 +1,9 @@
+package com.conditional;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        int num = 11;
+
+        if(num % 2 == 0) System.out.println("even");
+   }
+}
