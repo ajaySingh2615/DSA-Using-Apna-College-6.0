8a3471278e9ffc130c7280e5a0b505b3956cde16
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-31 21:12:41 +0530

    done with print numbers

--- src/com/conditional/LeapYear.java
+++ src/com/conditional/LeapYear.java
@@ -4,7 +4,7 @@ public class LeapYear {
     public static void main(String[] args) {
         int year = 1900;
 
-        if(((year % 4 == 0) || (year % 100 != 0)) && (year % 400 == 0)){
+        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
             System.out.println(year + " is a leap year");
         }else{
             System.out.println(year + " is not a leap year");
