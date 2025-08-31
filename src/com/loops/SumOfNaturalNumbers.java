10bdf7a3dccde350b88a9a46ad9bf8c946a5c614
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-31 21:16:37 +0530

    done with sum of natural numbers

---
+++ src/com/loops/SumOfNaturalNumbers.java
@@ -0,0 +1,14 @@
+package com.loops;
+
+class SumOfNaturalNumber{
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 0;
+
+        for (int i = 1; i <= num; i++) {
+               sum += i;
+        }
+
+        System.out.println("sum = " + sum);
+    }
+}
