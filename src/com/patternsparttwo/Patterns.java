8422c9167399c7b82c48b5b5fa8dd54aea592ad5
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-12 12:34:18 +0530

    done with patterns

--- src/com/patternsparttwo/Patterns.java
+++ src/com/patternsparttwo/Patterns.java
@@ -3,8 +3,76 @@ package com.patternsparttwo;
 public class Patterns {
     public static void main(String[] args) {
 //        printHollowRectangle(5);
+//        printInvertedRotatedHalfPyramid(4);
+//        printInvertedHalfPyramidWithNumber(5);
+//        printFloydTriangle(5);
+//        printZeroOneTriangle(5);
 
-        printInvertedRotatedHalfPyramid(4);
+        printButterflyPattern(8);
+    }
+
+    public static void printButterflyPattern(int n) {
+        for (int i = 1; i <= n / 2; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - (2 * i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n/2; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - (2 * i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printZeroOneTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printFloydTriangle(int n) {
+        int inc = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(inc);
+                inc++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printInvertedHalfPyramidWithNumber(int n) {
+        // outer
+        for (int i = 1; i <= n; i++) {
+            // inner
+            for (int j = 1; j <= (n + 1) - i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void printInvertedRotatedHalfPyramid(int n) {
